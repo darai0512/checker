41aa06184261a135e6b81b054525766a80d6debc
Daiki Arai <darai0512@yahoo.co.jp>
2025-01-11 14:50:59 +0900

    change logic: not recur but cursor

--- key.js
+++ key.js
@@ -2,48 +2,52 @@ import { execSync, exec } from 'node:child_process'
 import {
   setTimeout,
 } from 'node:timers/promises';
-// done 3 with -
-// done 4: abcdefghijklmnopqrstuvwxyz1234567890
-// mnopqrstu + v
-// 5: eeee* ~ enj3*, 数字のみ 000** - 027**, 111** - 669**
-// 7: eeeeee* ~ eeenjq*
-const chars = 'bcdefghijklmnopqrstuvwxyz1234567890a'.split('')
-
+const chars = 'abcdefghijklmnopqrstuvwxyz1234567890-'.split('')
+const lastIdx = chars.length - 1
+// multi-process, multi thread
 const reFirst3 = /^(.)\1\1/
 const re4 = /(.)\1\1\1+/
 
+let execKey = (process.env.KEY || 'lu7g0').trim()  // llml* - lu7g0
 const path = process.env.TARGET
-const start = process.env.START ? Number(process.env.START) : (new Date()).getHours() * 2
-for (let i = 0;i < start;i++) {
-  const key = chars.shift()
-  chars.push(key)
-}
-for (let len = 5;len<6;len++) {
-  console.log('check len: ', len)
-  await nestAsync('', len)
-  console.log('checked len: ', len)
-}
-// ssh-keygen -p -f path
-async function nestAsync(key, len) {
-  if (reFirst3.test(key)) return console.log('skip: ', key + '*'.repeat(len - key.length))
-  for (const c of chars) {
-    let execKey = key + c
-    if (re4.test(execKey)) {
-      // console.log('skip: ', execKey + '*'.repeat(len - execKey.length))
-      continue
-    } else if (execKey.length !== len) {
-      await nestAsync(execKey, len)
-      continue
-    }
-    await setTimeout(30)
-    exec(`ssh-keygen -y -P "${execKey}" -f ${path}`, (error, stdout, stderr) => {
-      if (error) {
-        if (!error.message.includes('incorrect passphrase supplied to decrypt private key')) console.log(execKey, error)
-        return
-      }
-      console.log(execKey, stdout)
-      process.exit(0);
-    })
+const idxes = [
+  chars.indexOf(execKey.slice(0, 1)) || 0,
+  chars.indexOf(execKey.slice(1, 2)) || 0,
+  chars.indexOf(execKey.slice(2, 3)) || 0,
+  chars.indexOf(execKey.slice(3, 4)) || 0,
+  chars.indexOf(execKey.slice(4, 5)) || 0,
+]
+
+function next(key, cursor) {
+  let index = idxes[cursor - 1]
+  if (index === lastIdx) {
+    idxes[cursor - 1] = 0
+    cursor -= 1
+    if (cursor < key.length - 1) console.log(key.slice(0, cursor) + '*'.repeat(key.length - cursor))
+    return next(key, cursor, chars.indexOf(key.slice(cursor - 1, cursor))) + chars[0]
   }
-  if (len - key.length >= 1) console.log(key + '*'.repeat(len - key.length))
+  index += 1
+  idxes[cursor - 1] = index
+  return key.slice(0, cursor-1) + chars[index]
+}
+
+
+const len = execKey.length
+while (true) {
+  execKey = next(execKey, len)
+
+  // skip
+  if (reFirst3.test(execKey) || re4.test(execKey)) continue
+
+  exec(`ssh-keygen -y -P "${execKey}" -f ${path}`, (error, stdout, stderr) => {
+    if (error) {
+      if (!error.message.includes('incorrect passphrase supplied to decrypt private key')) console.log(execKey, error)
+      return
+    }
+    console.log(execKey, stdout)
+    process.exit(0);
+  })
+  await setTimeout(20)
+
+  // log
 }
---
+++ recursive.js
@@ -0,0 +1,44 @@
+import { execSync, exec } from 'node:child_process'
+import {
+  setTimeout,
+} from 'node:timers/promises';
+// done 3 with -
+// done 4: abcdefghijklmnopqrstuvwxyz1234567890
+// mnopqrstu + v
+// 5: eeee* ~ enj3*, 数字のみ 000** - 027**, 111** - 669**
+const chars = 'bcdefghijklmnopqrstuvwxyz1234567890a'.split('')
+// multi-process, multi thread
+const reFirst3 = /^(.)\1\1/
+const re4 = /(.)\1\1\1+/
+
+const path = process.env.TARGET
+const start = process.env.START ? Number(process.env.START) : (new Date()).getHours() * 2
+for (let i = 0;i < start;i++) chars.push(chars.shift())
+for (let len = 5;len<6;len++) {
+  await nestAsync('', len)
+  console.log('checked len: ', len)
+}
+// ssh-keygen -p -f path
+async function nestAsync(key, len) {
+  if (reFirst3.test(key)) return console.log('skip: ', key + '*'.repeat(len - key.length))
+  for (const c of chars) {
+    let execKey = key + c
+    if (re4.test(execKey)) {
+      // console.log('skip: ', execKey + '*'.repeat(len - execKey.length))
+      continue
+    } else if (execKey.length !== len) {
+      await nestAsync(execKey, len)
+      continue
+    }
+    await setTimeout(30)
+    exec(`ssh-keygen -y -P "${execKey}" -f ${path}`, (error, stdout, stderr) => {
+      if (error) {
+        if (!error.message.includes('incorrect passphrase supplied to decrypt private key')) console.log(execKey, error)
+        return
+      }
+      console.log(execKey, stdout)
+      process.exit(0);
+    })
+  }
+  if (len - key.length >= 1) console.log(key + '*'.repeat(len - key.length))
+}
